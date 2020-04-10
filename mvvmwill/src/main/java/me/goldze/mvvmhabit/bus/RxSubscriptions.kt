ddd5072d83285dac81d9ab3aa976c71a6bef64fd
will <545512533@qq.com>
2020-04-10 23:49:57 +0800

    1、替换bus里面的类为kt

---
+++ mvvmwill/src/main/java/me/goldze/mvvmhabit/bus/RxSubscriptions.kt
@@ -0,0 +1,35 @@
+package me.goldze.mvvmhabit.bus
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+/**
+ * 管理 CompositeSubscription
+ */
+object RxSubscriptions {
+    private val mSubscriptions = CompositeDisposable()
+    val isDisposed: Boolean
+        get() = mSubscriptions.isDisposed
+
+    @JvmStatic
+    fun add(s: Disposable?) {
+        if (s != null) {
+            mSubscriptions.add(s)
+        }
+    }
+
+    @JvmStatic
+    fun remove(s: Disposable?) {
+        if (s != null) {
+            mSubscriptions.remove(s)
+        }
+    }
+
+    fun clear() {
+        mSubscriptions.clear()
+    }
+
+    fun dispose() {
+        mSubscriptions.dispose()
+    }
+}
