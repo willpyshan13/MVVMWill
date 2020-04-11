786efe57b93686eb38ae635ea0c4f5b03381b459
will <545512533@qq.com>
2020-04-11 23:03:55 +0800

    由于   转成kotlin 之后会有这个问题Unresolved reference: BR 添加apply plugin: "kotlin-kapt"  就可以了

---
+++ app/src/main/java/com/goldze/mvvmhabit/ui/form/FormViewModel.kt
@@ -0,0 +1,117 @@
+package com.goldze.mvvmhabit.ui.form
+
+import android.app.Application
+import android.text.TextUtils
+import android.view.View
+import androidx.databinding.ObservableBoolean
+import com.goldze.mvvmhabit.entity.FormEntity
+import com.goldze.mvvmhabit.entity.SpinnerItemData
+import com.goldze.mvvmhabit.repository.AppRepository
+import com.goldze.mvvmhabit.ui.base.viewmodel.ToolbarViewModel
+import com.google.gson.Gson
+import me.goldze.mvvmhabit.binding.command.BindingAction
+import me.goldze.mvvmhabit.binding.command.BindingCommand
+import me.goldze.mvvmhabit.binding.command.BindingConsumer
+import me.goldze.mvvmhabit.binding.viewadapter.spinner.IKeyAndValue
+import me.goldze.mvvmhabit.bus.event.SingleLiveEvent
+import me.goldze.mvvmhabit.utils.ToastUtils
+import java.util.*
+
+/**
+ * will
+ */
+class FormViewModel(application: Application) : ToolbarViewModel<AppRepository>(application) {
+    var entity: FormEntity? = null
+    var sexItemDatas: MutableList<IKeyAndValue>? = null
+    var entityJsonLiveData = SingleLiveEvent<String>()
+
+    //封装一个界面发生改变的观察者
+    var uc: UIChangeObservable? = null
+
+    inner class UIChangeObservable {
+        //显示日期对话框
+        var showDateDialogObservable: ObservableBoolean
+
+        init {
+            showDateDialogObservable = ObservableBoolean(false)
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        uc = UIChangeObservable()
+        //sexItemDatas 一般可以从本地Sqlite数据库中取出数据字典对象集合，让该对象实现IKeyAndValue接口
+        sexItemDatas = ArrayList()
+        (sexItemDatas as ArrayList<IKeyAndValue>).add(SpinnerItemData("男", "1"))
+        (sexItemDatas as ArrayList<IKeyAndValue>).add(SpinnerItemData("女", "2"))
+    }
+
+    /**
+     * 初始化Toolbar
+     */
+    fun initToolbar() {
+        //初始化标题栏
+        setRightTextVisible(View.VISIBLE)
+        if (TextUtils.isEmpty(entity!!.id)) {
+            //ID为空是新增
+            setTitleText("表单提交")
+        } else {
+            //ID不为空是修改
+            setTitleText("表单编辑")
+        }
+    }
+
+    public override fun rightTextOnClick() {
+        ToastUtils.showShort("更多")
+    }
+
+    fun setFormEntity(entity: FormEntity?) {
+        if (this.entity == null) {
+            this.entity = entity
+        }
+    }
+
+    //性别选择的监听
+    var onSexSelectorCommand = BindingCommand(object :BindingConsumer<IKeyAndValue>{
+        override fun call(t: IKeyAndValue) {
+
+        }
+
+    })
+
+
+
+    //生日选择的监听
+    var onBirClickCommand = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+            //回调到view层(Fragment)中显示日期对话框
+            uc!!.showDateDialogObservable.set(!uc!!.showDateDialogObservable.get())
+        }
+    })
+
+    //是否已婚Switch点状态改变回调
+    var onMarryCheckedChangeCommand = BindingCommand(object : BindingConsumer<Boolean> {
+        override fun call(isChecked: Boolean) {
+            entity!!.marry = isChecked
+        }
+    })
+
+    //提交按钮点击事件
+    var onCmtClickCommand: BindingCommand<*> = BindingCommand<Any?>(object : BindingAction {
+        override fun call() {
+            val submitJson = Gson().toJson(entity)
+            entityJsonLiveData.value = submitJson
+        }
+    })
+
+    fun setBir(year: Int, month: Int, dayOfMonth: Int) {
+        //设置数据到实体中，自动刷新界面
+        entity!!.bir = year.toString() + "年" + (month + 1) + "月" + dayOfMonth + "日"
+        //刷新实体,驱动界面更新
+        entity!!.notifyChange()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/goldze/mvvmhabit/ui/tab_bar/activity/TabBarViewModel.kt
+++ app/src/main/java/com/goldze/mvvmhabit/ui/tab_bar/activity/TabBarViewModel.kt
@@ -1,8 +1,9 @@
 package com.goldze.mvvmhabit.ui.tab_bar.activity
 
 import android.app.Application
+import com.goldze.mvvmhabit.repository.AppRepository
 import me.goldze.mvvmhabit.base.BaseViewModel
 
-//class TabBarViewModel(application: Application) : BaseViewModel<*>(application) {
-//
-//}
+class TabBarViewModel(application: Application) : BaseViewModel<AppRepository>(application) {
+
+}
