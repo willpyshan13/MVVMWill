ec3f0556827120d19e4e1d0e7d7ada39d0b9b9b4
will <545512533@qq.com>
2020-04-11 23:41:35 +0800

    修改recycleview 两个基础类

---
+++ mvvmwill/src/main/java/com/will/habit/binding/viewadapter/recyclerview/LineManagers.kt
@@ -0,0 +1,37 @@
+package com.will.habit.binding.viewadapter.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+
+/**
+ * @author will
+ */
+object LineManagers {
+    fun both(): LineManagerFactory {
+        return object : LineManagerFactory {
+            override fun create(recyclerView: RecyclerView): ItemDecoration {
+                return DividerLine(recyclerView.context, DividerLine.LineDrawMode.BOTH)
+            }
+        }
+    }
+
+    fun horizontal(): LineManagerFactory {
+        return object : LineManagerFactory {
+            override fun create(recyclerView: RecyclerView): ItemDecoration {
+                return com.will.habit.binding.viewadapter.recyclerview.DividerLine(recyclerView.context, com.will.habit.binding.viewadapter.recyclerview.DividerLine.LineDrawMode.HORIZONTAL)
+            }
+        }
+    }
+
+    fun vertical(): LineManagerFactory {
+        return object : LineManagerFactory {
+            override fun create(recyclerView: RecyclerView): ItemDecoration {
+                return com.will.habit.binding.viewadapter.recyclerview.DividerLine(recyclerView.context, com.will.habit.binding.viewadapter.recyclerview.DividerLine.LineDrawMode.VERTICAL)
+            }
+        }
+    }
+
+    interface LineManagerFactory {
+        fun create(recyclerView: RecyclerView): ItemDecoration
+    }
+}
