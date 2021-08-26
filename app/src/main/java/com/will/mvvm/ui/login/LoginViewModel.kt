3d5531d2c00084533d503fa8638fe71a67dc0fd0
will <545512533@qq.com>
2021-08-26 23:56:54 +0800

    1、textchange

--- app/src/main/java/com/will/mvvm/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/will/mvvm/ui/login/LoginViewModel.kt
@@ -1,17 +1,28 @@
 package com.will.mvvm.ui.login
 
 import android.app.Application
+import androidx.databinding.ObservableField
 import com.will.mvvm.base.BaseViewModel
 import com.will.mvvm.binding.command.BindingAction
 import com.will.mvvm.binding.command.BindingCommand
+import com.will.mvvm.binding.command.BindingConsumer
 import com.will.mvvm.repository.AppRepository
 import com.will.mvvm.utils.KLog
 
 class LoginViewModel(application: Application) : BaseViewModel<AppRepository>(application) {
+
+    val userName = ObservableField("")
 
     val edittextClick = BindingCommand<Any>(object : BindingAction {
         override fun call() {
             KLog.d("edittextClick")
         }
     })
+
+    val textChange = BindingCommand<String>(object:BindingConsumer<String>{
+        override fun call(t: String) {
+
+        }
+
+    } )
 }
